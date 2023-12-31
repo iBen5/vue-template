3cf9431de96072124e33744b2b8b072db68b1164
Ben <2316811795@qq.com>
2023-12-31 16:22:22 +0800

    chore: lint-staged配置

---
+++ .lintstagedrc.js
@@ -0,0 +1,17 @@
+import { ESLint } from 'eslint'
+
+const removeIgnoredFiles = async files => {
+  const eslint = new ESLint()
+  const ignoredFiles = await Promise.all(
+    files.map(file => eslint.isPathIgnored(file))
+  )
+  const filteredFiles = files.filter((_, i) => !ignoredFiles[i])
+  return filteredFiles.join(' ')
+}
+
+export default {
+  '*': async files => {
+    const filesToLint = await removeIgnoredFiles(files)
+    return [`eslint ${filesToLint} --max-warnings=0`]
+  }
+}
